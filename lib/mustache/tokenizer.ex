2e592300fa740a23a81fe3742c5317c4665b5ddf
Yuki Ito <yuki@gnnk.net>
2013-07-08 05:03:42 +0900

    Do not hard-code delimiters

--- lib/mustache/tokenizer.ex
+++ lib/mustache/tokenizer.ex
@@ -20,259 +20,237 @@ defmodule Mustache.Tokenizer do
   end
 
   def tokenize(list, line) when is_list(list) do
-    Enum.reverse(tokenize(list, line, line, [], []))
+    Enum.reverse(tokenize(list, { '{{', '}}' }, line, line, [], []))
   end
 
   ## private
 
   # tokenize
-  defp tokenize('{{{' ++ t, current_line, line, buffer, acc) do
-    acc = tokenize_text(current_line, buffer, acc)
-    { var, new_line, rest, _ } = tokenize_variable_for_triple(t, line, [])
 
-    cond do
-      var == :. ->
-        tokenize(rest, new_line, new_line, [], [{ :unescaped_dot, line, var } | acc])
-      to_binary(var) =~ %r/^\w+(\.\w+)+$/ ->
-        atoms = to_binary(var) |> String.split(".") |> Enum.map(binary_to_atom(&1))
-        tokenize(rest, new_line, new_line, [], [{ :unescaped_dotted_name, line, atoms } | acc])
-      true ->
-        tokenize(rest, new_line, new_line, [], [{ :unescaped_variable, line, var } | acc])
-    end
+  defp tokenize('\r\n' ++ t, tags, current_line, line, buffer, acc) do
+    tokenize(t, tags, current_line, line + 1, [?\n,?\r|buffer], acc)
   end
 
-  defp tokenize('{{!' ++ t, current_line, line, buffer, acc) do
-    ignore_break_flg = ignore_break?(buffer, acc)
-    { rest, new_line, ignore_tail_whitespace_flg } = tokenize_comment(t, line, ignore_break_flg)
-    acc = tokenize_text(current_line, buffer, acc, ignore_tail_whitespace_flg)
-
-    tokenize(rest, current_line, new_line, [], acc)
+  defp tokenize('\n' ++ t, tags, current_line, line, buffer, acc) do
+    tokenize(t, tags, current_line, line + 1, [?\n|buffer], acc)
   end
 
-  defp tokenize('{{&' ++ t, current_line, line, buffer, acc) do
-    acc = tokenize_text(current_line, buffer, acc)
-    { var, new_line, rest, _ } = tokenize_variable(t, line, [])
-
-    cond do
-      var == :. ->
-        tokenize(rest, new_line, new_line, [], [{ :unescaped_dot, line, var } | acc])
-      to_binary(var) =~ %r/^\w+(\.\w+)+$/ ->
-        atoms = to_binary(var) |> String.split(".") |> Enum.map(binary_to_atom(&1))
-        tokenize(rest, new_line, new_line, [], [{ :unescaped_dotted_name, line, atoms } | acc])
-      true ->
-        tokenize(rest, new_line, new_line, [], [{ :unescaped_variable, line, var } | acc])
-    end
-  end
-
-  defp tokenize('{{#' ++ t, current_line, line, buffer, acc) do
-    ignore_break_flg = ignore_break?(buffer, acc)
-    { var, new_line, rest, ignore_tail_whitespace_flg } = tokenize_variable(t, line, [], ignore_break_flg)
-    acc = tokenize_text(current_line, buffer, acc, ignore_tail_whitespace_flg)
-
-    if to_binary(var) =~ %r/^\w+(\.\w+)+$/ do
-      atoms = to_binary(var) |> String.split(".") |> Enum.map(binary_to_atom(&1))
-      tokenize(rest, new_line, new_line, [], [{ :dotted_name_section, line, atoms } | acc])
-    else
-      tokenize(rest, new_line, new_line, [], [{ :section, line, var } | acc])
-    end
-  end
-
-  defp tokenize('{{^' ++ t, current_line, line, buffer, acc) do
-    ignore_break_flg = ignore_break?(buffer, acc)
-    { var, new_line, rest, ignore_tail_whitespace_flg } = tokenize_variable(t, line, [], ignore_break_flg)
-    acc = tokenize_text(current_line, buffer, acc, ignore_tail_whitespace_flg)
-
-    if to_binary(var) =~ %r/^\w+(\.\w+)+$/ do
-      atoms = to_binary(var) |> String.split(".") |> Enum.map(binary_to_atom(&1))
-      tokenize(rest, new_line, new_line, [], [{ :dotted_name_inverted_section, line, atoms } | acc])
-    else
-      tokenize(rest, new_line, new_line, [], [{ :inverted_section, line, var } | acc])
-    end
-  end
-
-  defp tokenize('{{/' ++ t, current_line, line, buffer, acc) do
-    ignore_break_flg = ignore_break?(buffer, acc)
-    { var, new_line, rest, ignore_tail_whitespace_flg } = tokenize_variable(t, line, [], ignore_break_flg)
-    acc = tokenize_text(current_line, buffer, acc, ignore_tail_whitespace_flg)
-
-    if to_binary(var) =~ %r/^\w+(\.\w+)+$/ do
-      atoms = to_binary(var) |> String.split(".") |> Enum.map(binary_to_atom(&1))
-      tokenize(rest, new_line, new_line, [], [{ :end_section, line, atoms } | acc])
-    else
-      tokenize(rest, new_line, new_line, [], [{ :end_section, line, var } | acc])
-    end
-  end
-
- defp tokenize('{{>' ++ t, current_line, line, buffer, acc) do
-    ignore_break_flg = ignore_break?(buffer, acc)
-    { var, new_line, rest, ignore_tail_whitespace_flg } = tokenize_variable(t, line, [], ignore_break_flg)
-    acc = tokenize_text(current_line, buffer, acc, ignore_tail_whitespace_flg)
-
-    tokenize(rest, new_line, new_line, [], [{ :partial, line, var } | acc])
-  end
-
-
-  defp tokenize('{{' ++ t, current_line, line, buffer, acc) do
-    acc = tokenize_text(current_line, buffer, acc)
-    { var, new_line, rest, _ } = tokenize_variable(t, line, [])
-
-    cond do
-      var == :. ->
-        tokenize(rest, new_line, new_line, [], [{ :dot, line, var } | acc])
-      to_binary(var) =~ %r/^\w+(\.\w+)+$/ ->
-        atoms = to_binary(var) |> String.split(".") |> Enum.map(binary_to_atom(&1))
-        tokenize(rest, new_line, new_line, [], [{ :dotted_name, line, atoms } | acc])
-      true ->
-        tokenize(rest, new_line, new_line, [], [{ :variable, line, var } | acc])
-    end
-  end
-
-  defp tokenize('\r\n' ++ t, current_line, line, buffer, acc) do
-    tokenize(t, current_line, line + 1, [?\n,?\r|buffer], acc)
-  end
-
-  defp tokenize('\n' ++ t, current_line, line, buffer, acc) do
-    tokenize(t, current_line, line + 1, [?\n|buffer], acc)
-  end
-
-  defp tokenize([h|t], current_line, line, buffer, acc) do
-    tokenize(t, current_line, line, [h|buffer], acc)
-  end
-
-  defp tokenize([], current_line, _line, buffer, acc) do
+  defp tokenize([], _tags, current_line, _line, buffer, acc) do
     tokenize_text(current_line, buffer, acc)
+  end
+
+  defp tokenize(string, { otag, _ } = tags, current_line, line, buffer, acc) do
+    case strip_tag(string, otag) do
+      { :ok, rest } ->
+        case rest do
+          [] -> raise SyntaxError, description: "Illegal content in tag"
+          [?{|t] -> tokenize_mustache(t, tags, current_line, line, buffer, acc)
+          [?!|t] -> tokenize_bang(t, tags, current_line, line, buffer, acc)
+          [?#|t] -> tokenize_hash(t, tags, current_line, line, buffer, acc)
+          [?^|t] -> tokenize_hat(t, tags, current_line, line, buffer, acc)
+          [?/|t] -> tokenize_slash(t, tags, current_line, line, buffer, acc)
+          [?>|t] -> tokenize_gt(t, tags, current_line, line, buffer, acc)
+          [?&|t] -> tokenize_and(t, tags, current_line, line, buffer, acc)
+          t -> tokenize_simple(t, tags, current_line, line, buffer, acc)
+        end
+      _ ->
+        [h|t] = string
+        tokenize(t, tags, current_line, line, [h|buffer], acc)
+    end
+  end
+
+  #strip_tag
+
+  def strip_tag(rest, '') do
+    { :ok, rest }
+  end
+
+  def strip_tag('', _) do
+    nil
+  end
+
+  def strip_tag([h|t1], [h|t2]) do
+    strip_tag(t1, t2)
+  end
+
+  def strip_tag(_, _) do
+    nil
+  end
+
+  # extract kind
+
+  defp tokenize_mustache(t, { _, ctag } = tags, current_line, line, buffer, acc) do
+    acc = tokenize_text(current_line, buffer, acc)
+    { var, new_line, rest, _ } = tokenize_variable(t, [?}|ctag] , line, [])
+
+    cond do
+      var == :. ->
+        tokenize(rest, tags, new_line, new_line, [], [{ :unescaped_dot, line, var } | acc])
+      to_binary(var) =~ %r/^\w+(\.\w+)+$/ ->
+        atoms = to_binary(var) |> String.split(".") |> Enum.map(binary_to_atom(&1))
+        tokenize(rest, tags, new_line, new_line, [], [{ :unescaped_dotted_name, line, atoms } | acc])
+      true ->
+        tokenize(rest, tags, new_line, new_line, [], [{ :unescaped_variable, line, var } | acc])
+    end
+  end
+
+  defp tokenize_bang(t, { _, ctag } = tags, current_line, line, buffer, acc) do
+    ignore_break_flg = ignore_break?(buffer, acc)
+    { rest, new_line, ignore_tail_whitespace_flg } = tokenize_comment(t, ctag, line, ignore_break_flg)
+    acc = tokenize_text(current_line, buffer, acc, ignore_tail_whitespace_flg)
+
+    tokenize(rest, tags, current_line, new_line, [], acc)
+  end
+
+  defp tokenize_and(t, { _, ctag } = tags, current_line, line, buffer, acc) do
+    acc = tokenize_text(current_line, buffer, acc)
+    { var, new_line, rest, _ } = tokenize_variable(t, ctag, line, [])
+
+    cond do
+      var == :. ->
+        tokenize(rest, tags, new_line, new_line, [], [{ :unescaped_dot, line, var } | acc])
+      to_binary(var) =~ %r/^\w+(\.\w+)+$/ ->
+        atoms = to_binary(var) |> String.split(".") |> Enum.map(binary_to_atom(&1))
+        tokenize(rest, tags, new_line, new_line, [], [{ :unescaped_dotted_name, line, atoms } | acc])
+      true ->
+        tokenize(rest, tags, new_line, new_line, [], [{ :unescaped_variable, line, var } | acc])
+    end
+  end
+
+  defp tokenize_hash(t, { _, ctag } = tags, current_line, line, buffer, acc) do
+    ignore_break_flg = ignore_break?(buffer, acc)
+    { var, new_line, rest, ignore_tail_whitespace_flg } = tokenize_variable(t, ctag, line, [], ignore_break_flg)
+    acc = tokenize_text(current_line, buffer, acc, ignore_tail_whitespace_flg)
+
+    if to_binary(var) =~ %r/^\w+(\.\w+)+$/ do
+      atoms = to_binary(var) |> String.split(".") |> Enum.map(binary_to_atom(&1))
+      tokenize(rest, tags, new_line, new_line, [], [{ :dotted_name_section, line, atoms } | acc])
+    else
+      tokenize(rest, tags, new_line, new_line, [], [{ :section, line, var } | acc])
+    end
+  end
+
+  defp tokenize_hat(t, { _, ctag } = tags, current_line, line, buffer, acc) do
+    ignore_break_flg = ignore_break?(buffer, acc)
+    { var, new_line, rest, ignore_tail_whitespace_flg } = tokenize_variable(t, ctag, line, [], ignore_break_flg)
+    acc = tokenize_text(current_line, buffer, acc, ignore_tail_whitespace_flg)
+
+    if to_binary(var) =~ %r/^\w+(\.\w+)+$/ do
+      atoms = to_binary(var) |> String.split(".") |> Enum.map(binary_to_atom(&1))
+      tokenize(rest, tags, new_line, new_line, [], [{ :dotted_name_inverted_section, line, atoms } | acc])
+    else
+      tokenize(rest, tags, new_line, new_line, [], [{ :inverted_section, line, var } | acc])
+    end
+  end
+
+  defp tokenize_slash(t, { _, ctag } = tags, current_line, line, buffer, acc) do
+    ignore_break_flg = ignore_break?(buffer, acc)
+    { var, new_line, rest, ignore_tail_whitespace_flg } = tokenize_variable(t, ctag, line, [], ignore_break_flg)
+    acc = tokenize_text(current_line, buffer, acc, ignore_tail_whitespace_flg)
+
+    if to_binary(var) =~ %r/^\w+(\.\w+)+$/ do
+      atoms = to_binary(var) |> String.split(".") |> Enum.map(binary_to_atom(&1))
+      tokenize(rest, tags, new_line, new_line, [], [{ :end_section, line, atoms } | acc])
+    else
+      tokenize(rest, tags, new_line, new_line, [], [{ :end_section, line, var } | acc])
+    end
+  end
+
+ defp tokenize_gt(t, { _, ctag } = tags, current_line, line, buffer, acc) do
+    ignore_break_flg = ignore_break?(buffer, acc)
+    { var, new_line, rest, ignore_tail_whitespace_flg } = tokenize_variable(t, ctag, line, [], ignore_break_flg)
+    acc = tokenize_text(current_line, buffer, acc, ignore_tail_whitespace_flg)
+
+    tokenize(rest, tags, new_line, new_line, [], [{ :partial, line, var } | acc])
+  end
+
+
+  defp tokenize_simple(t, { _, ctag } = tags, current_line, line, buffer, acc) do
+    acc = tokenize_text(current_line, buffer, acc)
+    { var, new_line, rest, _ } = tokenize_variable(t, ctag, line, [])
+
+    cond do
+      var == :. ->
+        tokenize(rest, tags, new_line, new_line, [], [{ :dot, line, var } | acc])
+      to_binary(var) =~ %r/^\w+(\.\w+)+$/ ->
+        atoms = to_binary(var) |> String.split(".") |> Enum.map(binary_to_atom(&1))
+        tokenize(rest, tags, new_line, new_line, [], [{ :dotted_name, line, atoms } | acc])
+      true ->
+        tokenize(rest, tags, new_line, new_line, [], [{ :variable, line, var } | acc])
+    end
   end
 
   # tokenize comment
 
-  defp tokenize_comment('}}\r\n' ++ t, line, true) do
-    { t, line, true }
+  defp tokenize_comment([?\r,?\n|t], ctag, line, ignore_break_flg) do
+    tokenize_comment(t, ctag, line + 1, ignore_break_flg)
   end
 
-  defp tokenize_comment('}}\n' ++ t, line, true) do
-    { t, line, true }
+  defp tokenize_comment([?\n|t], ctag, line, ignore_break_flg) do
+    tokenize_comment(t, ctag, line + 1, ignore_break_flg)
   end
 
-  defp tokenize_comment('}}', line, true) do
-    { '', line, true }
-  end
-
-  defp tokenize_comment('}}' ++ t, line, _ignore_break_flg) do
-    { t, line, false }
-  end
-
-  defp tokenize_comment([?\r,?\n|t], line, ignore_break_flg) do
-    tokenize_comment(t, line + 1, ignore_break_flg)
-  end
-
-  defp tokenize_comment([?\n|t], line, ignore_break_flg) do
-    tokenize_comment(t, line + 1, ignore_break_flg)
-  end
-
-  defp tokenize_comment([_|t], line, ignore_break_flg) do
-    tokenize_comment(t, line, ignore_break_flg)
-  end
-
-  defp tokenize_comment([], line, _ignore_break_flg) do
+  defp tokenize_comment([], _ctag, line, _ignore_break_flg) do
     raise SyntaxError, line: line, description: "Unclosed tag"
+  end
+
+  defp tokenize_comment(string, ctag, line, ignore_break_flg) do
+    case strip_tag(string, ctag) do
+      { :ok, rest } ->
+        case rest do
+          [?\r,?\n|t] when ignore_break_flg -> { t, line, true }
+          [?\n|t] when ignore_break_flg -> { t, line, true }
+          [] when ignore_break_flg -> { '', line, true }
+          t -> { t, line, false }
+        end
+      _ ->
+        [_|t] = string
+        tokenize_comment(t, ctag, line, ignore_break_flg)
+    end
   end
 
   # tokenize variable
 
-  defp tokenize_variable(list, line, buffer, ignore_break_flg // false, finish_flg // false)
+  defp tokenize_variable(list, ctag, line, buffer, ignore_break_flg // false, finish_flg // false)
 
-  defp tokenize_variable('}}' ++ _, line, [], _ignore_break_flg, _finish_flg) do
-    raise SyntaxError, line: line, description: "No contents in tag"
+  defp tokenize_variable(' ' ++ t, ctag, line, [], ignore_break_flg, finish_flg) do
+    tokenize_variable(t, ctag, line, [], ignore_break_flg, finish_flg)
   end
 
-  defp tokenize_variable('}}\r\n' ++ t, line, buffer, true, _finish_flg) do
-    { buffer |> Enum.reverse |> list_to_atom, line, t, true }
+  defp tokenize_variable(' ' ++ t, ctag, line, buffer, ignore_break_flg, _finish_flg) do
+    tokenize_variable(t, ctag, line, buffer, ignore_break_flg, true)
   end
 
-  defp tokenize_variable('}}\n' ++ t, line, buffer, true, _finish_flg) do
-    { buffer |> Enum.reverse |> list_to_atom, line, t, true }
-  end
-
-  defp tokenize_variable('}}', line, buffer, true, _finish_flg) do
-    { buffer |> Enum.reverse |> list_to_atom, line, '', true }
-  end
-
-  defp tokenize_variable('}}' ++ t, line, buffer, _ignore_break_flg, _finish_flg) do
-    { buffer |> Enum.reverse |> list_to_atom, line, t, false }
-  end
-
-  defp tokenize_variable(' ' ++ t, line, [], ignore_break_flg, finish_flg) do
-    tokenize_variable(t, line, [], ignore_break_flg, finish_flg)
-  end
-
-  defp tokenize_variable(' ' ++ t, line, buffer, ignore_break_flg, _finish_flg) do
-    tokenize_variable(t, line, buffer, ignore_break_flg, true)
-  end
-
-  defp tokenize_variable([_h|_], line, _buffer, _ignore_break_flg, true) do
-    raise SyntaxError, line: line, description: "Illegal content in tag #{inspect(" ")}"
-  end
-
-  defp tokenize_variable([h|t], line, buffer, ignore_break_flg, finish_flg) when h in ?a..?z or h in ?A..?Z or h in ?0..?9 or h in [?_, ?-, ?/, ?!, ??, ?.] do
-    tokenize_variable(t, line, [h|buffer], ignore_break_flg, finish_flg)
-  end
-
-  defp tokenize_variable([h|_], line, _buffer, _ignore_break_flg, _finish_flg) do
-    raise SyntaxError, line: line, description: "Illegal content in tag #{inspect(<<h>>)}"
-  end
-
-  defp tokenize_variable([], line, _buffer, _ignore_break_flg, _finish_flg) do
+  defp tokenize_variable([], _ctag, line, _buffer, _ignore_break_flg, _finish_flg) do
     raise SyntaxError, line: line, description: "Unclosed tag"
   end
 
-  # same with tokenize_variable
-  defp tokenize_variable_for_triple(list, line, buffer, ignore_break_flg // false, finish_flg // false)
-
-  defp tokenize_variable_for_triple('}}}' ++ _, line, [], _ignore_break_flg, _finish_flg) do
-    raise SyntaxError, line: line, description: "No contents in tag"
+  defp tokenize_variable(string, ctag, line, buffer, ignore_break_flg, finish_flg) do
+    case strip_tag(string, ctag) do
+      { :ok, rest } ->
+        case rest do
+          _ when buffer == [] ->
+            raise SyntaxError, line: line, description: "No contents in tag"
+          [?\r,?\n|t] when ignore_break_flg ->
+            { buffer |> Enum.reverse |> list_to_atom, line, t, true }
+          [?\n|t] when ignore_break_flg ->
+            { buffer |> Enum.reverse |> list_to_atom, line, t, true }
+          [] when ignore_break_flg ->
+            { buffer |> Enum.reverse |> list_to_atom, line, '', true }
+          t ->
+            { buffer |> Enum.reverse |> list_to_atom, line, t, false }
+        end
+      _ ->
+        case string do
+          [_|_] when finish_flg ->
+            raise SyntaxError, line: line, description: "Illegal content in tag #{inspect(" ")}"
+          [h|t] when h in ?a..?z or h in ?A..?Z or h in ?0..?9 or h in [?_, ?-, ?/, ?!, ??, ?.] ->
+            tokenize_variable(t, ctag, line, [h|buffer], ignore_break_flg, finish_flg)
+          [h|_] ->
+            raise SyntaxError, line: line, description: "Illegal content in tag #{inspect(<<h>>)}"
+        end
+    end
   end
-
-  defp tokenize_variable_for_triple('}}}\r\n' ++ t, line, buffer, true, _finish_flg) do
-    { buffer |> Enum.reverse |> list_to_atom, line, t, true }
-  end
-
-  defp tokenize_variable_for_triple('}}}\n' ++ t, line, buffer, true, _finish_flg) do
-    { buffer |> Enum.reverse |> list_to_atom, line, t, true }
-  end
-
-  defp tokenize_variable_for_triple('}}}', line, buffer, true, _finish_flg) do
-    { buffer |> Enum.reverse |> list_to_atom, line, '', true }
-  end
-
-  defp tokenize_variable_for_triple('}}}' ++ t, line, buffer, _ignore_break_flg, _finish_flg) do
-    { buffer |> Enum.reverse |> list_to_atom, line, t, false }
-  end
-
-  defp tokenize_variable_for_triple(' ' ++ t, line, [], ignore_break_flg, finish_flg) do
-    tokenize_variable_for_triple(t, line, [], ignore_break_flg, finish_flg)
-  end
-
-  defp tokenize_variable_for_triple(' ' ++ t, line, buffer, ignore_break_flg, _finish_flg) do
-    tokenize_variable_for_triple(t, line, buffer, ignore_break_flg, true)
-  end
-
-  defp tokenize_variable_for_triple([_h|_], line, _buffer, _ignore_break_flg, true) do
-    raise SyntaxError, line: line, description: "Illegal content in tag #{inspect(" ")}"
-  end
-
-  defp tokenize_variable_for_triple([h|t], line, buffer, ignore_break_flg, finish_flg) when h in ?a..?z or h in ?A..?Z or h in ?0..?9 or h in [?_, ?-, ?/, ?!, ??, ?.] do
-    tokenize_variable_for_triple(t, line, [h|buffer], ignore_break_flg, finish_flg)
-  end
-
-  defp tokenize_variable_for_triple([h|_], line, _buffer, _ignore_break_flg, _finish_flg) do
-    raise SyntaxError, line: line, description: "Illegal content in tag #{inspect(<<h>>)}"
-  end
-
-  defp tokenize_variable_for_triple([], line, _buffer, _ignore_break_flg, _finish_flg) do
-    raise SyntaxError, line: line, description: "Unclosed tag"
-  end
-
-
 
   # tokenize text
 
