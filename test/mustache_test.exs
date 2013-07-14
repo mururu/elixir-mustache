3b4ca33b0a4f705b3b9f669fad6da1b640afc397
Yuki Ito <yuki@gnnk.net>
2013-07-14 22:29:41 +0900

    Rewrite tests

--- test/mustache_test.exs
+++ test/mustache_test.exs
@@ -3,34 +3,130 @@ Code.require_file "test_helper.exs", __DIR__
 defmodule MustacheTest do
   use ExUnit.Case
 
-  test "1",  do: assert Mustache.render("{{ a }}", a: 1) == "1"
-  test "2",  do: assert Mustache.render("{{ a }}", a: "&") == "&amp;"
-  test "3",  do: assert Mustache.render("{{& a }}", a: "&") == "&"
-  test "4",  do: assert Mustache.render("{{# a }}a{{/ a }}", a: true) == "a"
-  test "5",  do: assert Mustache.render("{{# a }}a{{/ a }}", a: false) == ""
-  test "6",  do: assert Mustache.render("{{# a }}a{{/ a }}") == ""
-  test "7",  do: assert Mustache.render("{{# a }}a{{/ a }}", a: []) == ""
-  test "8",  do: assert Mustache.render("{{# a }}a{{/ a }}", a: [[]]) == "a"
-  test "9",  do: assert Mustache.render("{{# a }}a{{/ a }}", a: [1,2]) == "aa"
-  test "10", do: assert Mustache.render("{{# a }}{{ b }}{{/ a }}", a: [[b: 3], [b: 3]]) == "33"
-  test "11", do: assert Mustache.render("{{# a }}{{ b }}1{{/ a }}", a: [[], [b: "&"]]) == "1&amp;1"
-  test "12", do: assert Mustache.render("{{^ a }}1{{/ a }}", []) == "1"
-  test "13", do: assert Mustache.render("{{^ a }}1{{/ a }}", [a: false]) == "1"
-  test "14", do: assert Mustache.render("{{^ a }}1{{/ a }}", [a: []]) == "1"
-  test "15", do: assert Mustache.render("{{^ a }}1{{/ a }}", [a: true]) == ""
-  test "16", do: assert Mustache.render("{{^ a }}1{{/ a }}", [a: [1]]) == ""
-  test "17", do: assert Mustache.render("{{# a }}{{b}}{{/ a }}", a: [b: 2]) == "2"
-  test "18", do: assert Mustache.render("{{# a }}{{b}}{{c}}{{/ a }}", a: [b: 2, c: 3]) == "23"
-  test "19", do: assert Mustache.render("{{^ a }}{{b}}{{/ a }}", a: [b: 2]) == ""
-  test "20" do
+  test "render simple" do
+    assert Mustache.render("Hello, {{name}}", [name: "Mustache"]) == "Hello, Mustache"
+  end
+
+  test "render multi line" do
+    assert Mustache.render("Hello\n{{name}}", [name: "Mustache"]) == "Hello\nMustache"
+  end
+
+  test "render with nested context" do
+    assert Mustache.render("{{#a}}{{#b}}Mustache{{/b}}{{/a}}", [a: true, b: true]) == "Mustache"
+  end
+
+  test "render nil" do
+    assert Mustache.render("Hello, {{name}}", [name: nil]) == "Hello, "
+  end
+  test "render missing variable" do
+    assert Mustache.render("Hello, {{name}}", []) == "Hello, "
+  end
+
+  test "render unescaped" do
+    assert Mustache.render("{{{string}}}", [string: "&\'\"<>"]) == "&\'\"<>"
+  end
+
+  test "render unescaped ampersand" do
+    assert Mustache.render("{{{string}}}", [string: "&\'\"<>"]) == "&\'\"<>"
+  end
+
+  test "render escaped" do
+    assert Mustache.render("{{string}}", [string: "&\'\"<>"]) == "&amp;&#39;&quot;&lt;&gt;"
+  end
+
+  test "render list" do
+    assert Mustache.render("Hello{{#names}}, {{name}}{{/names}}", [names: [[name: "Mustache"], [name: "Elixir"]]]) == "Hello, Mustache, Elixir"
+  end
+
+  test "render list twice" do
+    assert Mustache.render("Hello{{#names}}, {{name}}{{/names}}! Hello{{#names}}, {{name}}{{/names}}!", [names: [[name: "Mustache"], [name: "Elixir"]]]) == "Hello, Mustache, Elixir! Hello, Mustache, Elixir!"
+  end
+
+  test "render single value" do
+    assert Mustache.render("Hello{{#person}}, {{name}}{{/person}}!", [person: [name: "Mustache"]]) == "Hello, Mustache!"
+  end
+
+  test "render empty list" do
+    assert Mustache.render("{{#things}}something{{/things}}", [things: []]) == ""
+  end
+
+  test "render nested list" do
+    assert Mustache.render("{{#x}}{{#y}}{{z}}{{/y}}{{/x}}", [x: [y: [z: "z"]]]) == "z"
+  end
+
+  test "render comment" do
+    assert Mustache.render("Hello, {{! comment }}{{name}}", [name: "Mustache"]) == "Hello, Mustache"
+  end
+
+  test "render tags with whitespace" do
+    assert Mustache.render("Hello, {{# names }}{{ name }}{{/ names }}", [names: [[name: "Mustache"]]]) == "Hello, Mustache"
+  end
+
+  test "render true section" do
+    assert Mustache.render("Hello, {{#bool}}Mustache{{/bool}}", [bool: true]) == "Hello, Mustache"
+  end
+
+  test "render false section" do
+    assert Mustache.render("Hello, {{#bool}}Mustache{{/bool}}", [bool: false]) == "Hello, "
+  end
+
+  test "render inverted empty list" do
+    assert Mustache.render("{{^things}}Empty{{/things}}", [thins: []]) == "Empty"
+  end
+
+  test "render inverted list" do
+    assert Mustache.render("{{^things}}Empty{{/things}}", [things: ["yeah"]]) == ""
+  end
+
+  test "render inverted true section" do
+    assert Mustache.render("Hello, {{^bool}}Mustache{{/bool}}", [bool: true]) == "Hello, "
+  end
+
+  test "render inverted false section" do
+    assert Mustache.render("Hello, {{^bool}}Mustache{{/bool}}", [bool: false]) == "Hello, Mustache"
+  end
+
+  test "render with delimiters" do
+    assert Mustache.render("{{=<% %>=}}Hello, <%name%>", [name: "Mustache"]) == "Hello, Mustache"
+  end
+
+  test "render with delimiters changed twice" do
+    assert Mustache.render("{{=[ ]=}}[greeting], [=<% %>=]<%name%>", [greeting: "Hello", name: "Mustache"]) == "Hello, Mustache"
+  end
+
+  test "render dotted name" do
+    assert Mustache.render("Hello, {{cool.mustache.name}}", [cool: [mustache: [name: "Mustache"]]]) == "Hello, Mustache"
+  end
+
+  test "render dotted name section" do
+    assert Mustache.render("Hello, {{#person.name}}Mustache{{/person.name}}", [person: [name: true]]) == "Hello, Mustache"
+  end
+
+  test "render dotted name inverted section" do
+    assert Mustache.render("Hello, {{#person.name}}Mustache{{/person.name}}", [people: [names: true]]) == "Hello, "
+  end
+
+  test "render implicit iterator" do
+    assert Mustache.render("Hello{{#names}}, {{.}}{{/names}}!", [names: ["Mustache", "Elixir"]]) == "Hello, Mustache, Elixir!"
+  end
+
+  test "render lambda" do
+    assert Mustache.render("Hello, {{name}}", [name: fn -> "Mustache" end]) == "Hello, Mustache"
+  end
+
+  test "render partial" do
+    assert Mustache.render("Hello, {{>name}}", [n: "Mustache"], partials: [name: "{{n}}"]) == "Hello, Mustache"
+  end
+
+  test "render looooong" do
     template = """
     {{! ignore this line! }}
     Hello {{name}}
-    {{! ignore this line! }}
+      {{! ignore this line! }}
     You have just won {{value}} dollars!
-    {{#in_ca}}
+      {{#in_ca}}
     Well, {{taxed_value}} dollars, after taxes.
-    {{/in_ca}}
+      {{/in_ca}}
     a{{! dont ignore this line! }}
     """
 
@@ -44,11 +140,4 @@ defmodule MustacheTest do
 
     assert Mustache.render(template, [name: "mururu", value: 1000, in_ca: [[taxed_value: 50], [taxed_value: 40]]]) == expected
   end
-  test "21", do: assert Mustache.render("{{#a}}{{.}}{{/a}}", a: [1,2,3]) == "123"
-  test "22", do: assert Mustache.render("{{a.b}}", a: [b: 1]) == "1"
-  test "23", do: assert Mustache.render("{{a.b}}", a: [[b: 1]]) == ""
-  test "24", do: assert Mustache.render("{{#a.b.c}}1{{/a.b.c}}", a: [b: [c: true]]) == "1"
-  test "25", do: assert Mustache.render("{{^a.b.c}}1{{/a.b.c}}", a: [b: [c: true]]) == ""
-  test "26", do: assert Mustache.render("{{a}}{{=<% %>=}}<%a%><%={{ }}=%>{{a}}", a: 1) == "111"
-  test "27", do: assert Mustache.render("{{#a}}{{#b}}1{{/b}}{{/a}}", [a: true, b: true]) == "1"
 end
