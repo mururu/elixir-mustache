2e1698c08f36519f2b1a73a74d9aa95f962915ce
Yuki Ito <yuki@gnnk.net>
2013-07-15 01:27:59 +0900

    Add docs for dynamo

--- lib/mustache/dynamo/mustache_handler.ex
+++ lib/mustache/dynamo/mustache_handler.ex
@@ -1,8 +1,50 @@
 if Enum.all? [Dynamo.Templates.Handler, Dynamo.Template], Code.ensure_loaded?(&1) do
   defmodule Dynamo.Templates.MUSTACHEHandler do
-    @moduledoc false
+    @moduledoc """
+    Mustache Handler for [Dynamo](https://github.com/elixir-lang/dynamo)
+
+      ## Examples
+
+        # hello.html.mustache
+        <html>
+        <head>
+          <title>{{title}}</title>
+        </head>
+        <body>
+          <ul>
+          {{#contents}}
+            <li>{{.}}</li>
+          {{/contents}}
+          </ul>
+        </body>
+        </html>
+
+        # router
+        get "/" do
+          conn.assign(:title, "Mustache")
+          conn.assign(:contents, ["first", "second"])
+          render conn, "hello.html"
+        end
+
+        # result
+        <!DOCTYPE HTML>
+        <html>
+        <head>
+          <title>Mustache</title>
+        </head>
+        <body>
+          <ul>
+            <li>first</li>
+            <li>second</li>
+          <ul>
+        </body>
+        </html>
+
+    """
+
     @behaviour Dynamo.Templates.Handler
 
+    @doc false
     def compile(Dynamo.Template[identifier: identifier], source, locals) do
       vars   = vars(locals)
       args   = [{ :mustache_root, [], nil }|vars]
@@ -16,6 +58,7 @@ if Enum.all? [Dynamo.Templates.Handler, Dynamo.Template], Code.ensure_loaded?(&1
       end }
     end
 
+    @doc false
     def render(module, function, locals, assigns) do
       apply module, function, [assigns|Keyword.values(locals)]
     end
